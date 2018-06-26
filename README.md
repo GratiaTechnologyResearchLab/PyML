609d2729801fd6d5461c3f7155458eeeb947f8e5
Hridoy Dutta <hridaydutta123@gmail.com>
2018-06-27 00:51:15 +0530

    Added README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python + ML Gratia Technology Training/Internship Repo
+
---
+++ classes/doctest_tutorial.py
@@ -0,0 +1,8 @@
+# Custom function to add list
+def addList(list_):
+    '''
+    >>> x = [1,2,3]
+    >>> addList(x)
+    6
+    '''
+    return sum(list_)
